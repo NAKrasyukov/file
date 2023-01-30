2be27d9e0529da7a30af67ee5f5328ee250dcbf6
NAKrasyukov <123346987+NAKrasyukov@users.noreply.github.com>
2023-01-30 12:48:38 +0600

    Updated file

--- Lab2.py
+++ Lab2.py
@@ -1,21 +1,31 @@
 name = "a"
 course = 0
 university = "B"
+#Цикл "слушающий" команды
 while True:
-    print("Input variable")
+    print("Input comand")
     q = input()
+    # name
     if q == "name":
         print("Input name: ")
         name = input()
+    #university
     elif q == "university":
         print("Input university: ")
         university = input()
+    #course
     elif q == "course":
         print("Input course: ")
         course = input()
+    #показывает все переменные
     elif q == "print":
         print("name: "+ name)
         print("course: "+ course)
         print("university: "+ university)
+    #exit
+    elif q == "exit":
+        print("Bye!")
+        break
+    #не верная команда
     else:
         print("Unknown variable")
